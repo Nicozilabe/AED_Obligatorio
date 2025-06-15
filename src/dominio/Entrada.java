c0b6ef07fa4c91d0f86de545dc4c7acffaf1af90
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-06-15 16:39:18 -0300

    Add esValida property and devolverEntrada method to Entrada class for entry validation management

--- src/dominio/Entrada.java
+++ src/dominio/Entrada.java
@@ -11,17 +11,27 @@ package dominio;
 public class Entrada implements Comparable<Entrada>{
     private Cliente cliente;
     private Evento evento;
+    private boolean esValida;
 
     
     public Entrada(Cliente cliente, Evento evento) {
         this.cliente = cliente;
         this.evento = evento;
+        this.esValida = true; 
     }
 
 
     public Cliente getCliente() {
         return cliente;
     }
+
+
+    public void devolverEntrada() {
+        this.esValida = false;
+    }
+
+
+
 
     @Override
     public int compareTo(Entrada o) {
@@ -35,5 +45,11 @@ public class Entrada implements Comparable<Entrada>{
         return evento.toString() + " - " + cliente.toString();
     }
 
+
+    @Override
+    public boolean equals(Object obj) {
+        Entrada other = (Entrada) obj;
+        return this.cliente.equals(other.cliente) && this.evento.equals(other.evento);
+    }
     //evaluar propiedad boolean EsValida
 }
